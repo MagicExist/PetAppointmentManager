c7eccdc949d85fe44351216001c630819138223a
MagicExist <johhanparra1@gmail.com>
2025-07-16 16:21:29 -0500

    (front): implement react routert

--- front-end/PetAppointmentManager/src/App.jsx
+++ front-end/PetAppointmentManager/src/App.jsx
@@ -1,14 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import AppointmentReadView from './components/AppointmentsComponents/AppointmentReadView/AppointmentReadView'
 import NavBarComponent from './components/NavBarComponent/NavBarComponent'
+import PetReadView from './components/PetsComponents/PetReadView/PetReadView';
 
 function App() {
 
   return (
-    <>
+    <Router>
       <NavBarComponent/>
-      <AppointmentReadView></AppointmentReadView>
-    </>
+      <Routes>
+        <Route path='/' element={<AppointmentReadView/>}/>
+        <Route path='/pet' element={<PetReadView/>}/>
+      </Routes>
+    </Router>
   )
 }
 
--- front-end/PetAppointmentManager/src/components/NavBarComponent/NavBarComponent.jsx
+++ front-end/PetAppointmentManager/src/components/NavBarComponent/NavBarComponent.jsx
@@ -1,16 +1,16 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 
 export default function NavBarComponent(){
     return(
         <Navbar bg="dark" variant="dark" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">My App</Navbar.Brand>
+                <Navbar.Brand as={Link} to={"/"}>Appointment</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#about">About</Nav.Link>
+                    <Nav.Link as={Link} to={"/pet"}>Pet</Nav.Link>
                 </Nav>
                 </Navbar.Collapse>
             </Container>
