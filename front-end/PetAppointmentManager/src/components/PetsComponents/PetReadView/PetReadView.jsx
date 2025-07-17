be14cb5ee80e30ff56deed317009549584487649
MagicExist <johhanparra1@gmail.com>
2025-07-17 15:49:53 -0500

    (front): add button to navigate into pet creation page

--- front-end/PetAppointmentManager/src/components/PetsComponents/PetReadView/PetReadView.jsx
+++ front-end/PetAppointmentManager/src/components/PetsComponents/PetReadView/PetReadView.jsx
@@ -1,12 +1,15 @@
 import PetCardComponent from "../PetCardComponent/PetCardComponent";
-
+import './PetReadView.css'
 
 export default function PetReadView(){
 
     return (
         <>
             <div className='container text-white'>
-                <h1 className='mb-3 mt-3'>Pets</h1>
+                <div className="d-flex align-items-center">
+                    <h1 className='mb-3 mt-3 me-3'>Pets</h1>
+                    <button id="addPetBtn" className="btn btn-success rounded-circle d-flex align-items-center p-1"><img src="/add-symbol.svg" alt="Add" className="img-fluid"/></button>
+                </div>
                 <PetCardComponent></PetCardComponent>
             </div>
         </>
