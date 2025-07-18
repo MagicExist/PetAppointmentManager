8948a0dd3c1bc5a6d989551e2f8623fa7b22f211
MagicExist <johhanparra1@gmail.com>
2025-07-18 16:49:54 -0500

    (front): add Appointment Create View

--- front-end/PetAppointmentManager/src/App.jsx
+++ front-end/PetAppointmentManager/src/App.jsx
@@ -5,6 +5,7 @@ import NavBarComponent from './components/NavBarComponent/NavBarComponent'
 import PetReadView from './components/PetsComponents/PetReadView/PetReadView';
 import PetCreateView from './components/PetsComponents/PetCreateView/PetCreateView';
 import PetEditView from './components/PetsComponents/PetEditView/PetEditView';
+import AppointmentCreateView from './components/AppointmentsComponents/AppointmentCreateView/AppointmentCreateView';
 
 function App() {
 
@@ -13,6 +14,8 @@ function App() {
       <NavBarComponent/>
       <Routes>
         <Route path='/' element={<AppointmentReadView/>}/>
+        <Route path='/appointment/create' element={<AppointmentCreateView/>}/>
+
         <Route path='/pet' element={<PetReadView/>}/>
         <Route path='/pet/create' element={<PetCreateView/>}/>
         <Route path='/pet/edit/:id' element={<PetEditView/>}/>
