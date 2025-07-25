91c3e2fd4dc0d073d7c135bf0104de8ab9316716
MagicExist <johhanparra1@gmail.com>
2025-07-25 17:02:01 -0500

    (front-end) add login view

--- front-end/PetAppointmentManager/src/components/LoginComponents/LoginView/LoginView.jsx
+++ front-end/PetAppointmentManager/src/components/LoginComponents/LoginView/LoginView.jsx
@@ -1,11 +1,27 @@
-
+import './LoginView.css'
 
 
 export default function LoginView(){
 
     return(
-        <>
-            <h1>Login</h1>
-        </>
+        <div className="container  d-flex justify-content-center align-items-center mainContainer text-white">
+            <div className="row rowContainer justify-content-center w-50 flex-column rounded-4">
+                <div className='col d-flex flex-column align-items-center'>
+                    <h1 className='mb-5 mt-5'>Login</h1>
+                    <form className='d-flex flex-column align-items-center w-100 mt-5 mb-5' method='post'>
+                        <div className='d-flex flex-column w-100  align-items-center justify-content-center mb-4'>
+                            <label htmlFor='inputUsername' className='mb-2 fs-5 loginLabel'>Username</label>
+                            <input placeholder='Username' id='inputUsername' className='form-control w-25'/>
+                        </div>
+                        <div className='d-flex flex-column w-100  align-items-center justify-content-center'>
+                            <label htmlFor='inputPassword' className='mb-2 fs-5 loginLabel'>Password</label>
+                            <input type='password' placeholder='Password' id='inputPassword' className='form-control w-25'/>
+                        </div>
+                    </form>
+                    <button className='btn btn-primary loginBtn'>Login</button>
+                </div>
+                
+            </div>
+        </div>
     )
 }
