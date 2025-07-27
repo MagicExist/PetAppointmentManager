fbe8da2cc9b4c93b12cf0bf49308fa85bff055df
MagicExist <johhanparra1@gmail.com>
2025-07-27 17:05:07 -0500

    (fix): fix core app name

--- back-end/PetAppointmentManagerApi/PetAppointmentManagerApi/settings.py
+++ back-end/PetAppointmentManagerApi/PetAppointmentManagerApi/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'PetAppointmentManagerApi.Core',
+    'Core.apps.CoreConfig',
     'rest_framework',
     'corsheaders',
     'rest_framework_simplejwt',
