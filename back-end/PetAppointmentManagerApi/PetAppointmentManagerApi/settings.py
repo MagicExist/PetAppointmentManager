25da85f9f8db95deadca6d0dc5271f5b04b98896
MagicExist <johhanparra1@gmail.com>
2025-07-27 16:03:10 -0500

    (fix): fix core app location

--- back-end/PetAppointmentManagerApi/PetAppointmentManagerApi/settings.py
+++ back-end/PetAppointmentManagerApi/PetAppointmentManagerApi/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Core',
+    'PetAppointmentManagerApi.Core',
     'rest_framework',
     'corsheaders',
     'rest_framework_simplejwt',
